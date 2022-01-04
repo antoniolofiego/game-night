a0ad7c60f246db05ed11ca303dc5e9c0ff9729bd
Antonio Lo Fiego <alofiego@hey.com>
2022-01-04 17:55:31 -0500

    Temporary typing for deployment test

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -5,11 +5,12 @@ import { parseString } from 'xml2js';
 const BASE_URL = 'https://www.boardgamegeek.com/xmlapi2';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	//TODO: fix typing
 	const query = req.body.query;
 	const URL = `${BASE_URL}/search?query=${query}&type=boardgame`;
 
-	const transformSearchResults = (searchResults) => {
-		return searchResults.map((game) => {
+	const transformSearchResults = (searchResults: any) => {
+		return searchResults.map((game: any) => {
 			const bgg_id = parseInt(game.$.id);
 			const name = game.name ? game.name[0].$.value : '';
 
@@ -20,7 +21,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 		});
 	};
 
-	let searchResults;
+	let searchResults: any;
 
 	const fetchSearchResults = async (URL: string) => {
 		const response = await axios.get(URL, {

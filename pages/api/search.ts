888150f4cc80bc0b3d87fffef228997b4a753c40
Antonio Lo Fiego <alofiego@hey.com>
2022-01-03 23:23:38 -0500

    Initial search endpoint

---
+++ pages/api/search.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import { parseString } from 'xml2js';
+
+const BASE_URL = 'https://www.boardgamegeek.com/xmlapi2';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const query = req.body.query;
+	const URL = `${BASE_URL}/search?query=${query}&type=boardgame`;
+
+	const transformSearchResults = (searchResults) => {
+		return searchResults.map((game) => {
+			const bgg_id = parseInt(game.$.id);
+			const name = game.name ? game.name[0].$.value : '';
+
+			return {
+				bgg_id,
+				name,
+			};
+		});
+	};
+
+	let searchResults;
+
+	const fetchSearchResults = async (URL: string) => {
+		const response = await axios.get(URL, {
+			responseType: 'text',
+		});
+
+		try {
+			parseString(response.data, (err, result) => {
+				searchResults = result?.items.item;
+			});
+
+			res.send(transformSearchResults(searchResults));
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	await fetchSearchResults(URL);
+};
+
+export default handler;

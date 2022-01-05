a18574b935fea3ffcdc0c8db2f53b8a00c538d48
Antonio Lo Fiego <alofiego@hey.com>
2022-01-05 01:22:14 -0500

    Move Game type definition to its own file

---
+++ _types/Game.ts
@@ -0,0 +1,15 @@
+export type Game = {
+	bgg_id: number;
+	name: string;
+	minPlayers: number;
+	maxPlayers: number;
+	thumbnail: string;
+	image: string;
+	description: string;
+	playingTime: number;
+	rating: number;
+	bgg_rank: number | null;
+	weight: number;
+	mechanics: string[];
+	categories: string[];
+};

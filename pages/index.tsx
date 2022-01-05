a3c4582c79910e215ef237f48831664c9c95b492
Antonio Lo Fiego <alofiego@hey.com>
2022-01-05 01:24:41 -0500

    Basic implementation of collection retrieval

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import Head from 'next/head';
+import { supabase } from '@utils/supabase';
+import { Game } from '_types/Game';
 
-const Home: React.FC = () => {
+type PropType = {
+	collection: Game[];
+};
+
+const Home: React.FC<PropType> = ({ collection }) => {
+	console.log(collection);
 	return (
 		<>
 			<Head>
@@ -20,3 +27,20 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const { data: userCollection, error } = await supabase
+		.from('userCollections')
+		.select(`boardgames(*)`)
+		.eq('user_id', 'c4814033-4979-4b10-9f0d-cf0b0c6cb4f5');
+
+	console.log(userCollection?.length);
+
+	const parsedCollection = userCollection?.map((game) => game.boardgames);
+
+	return {
+		props: {
+			collection: parsedCollection,
+		},
+	};
+};

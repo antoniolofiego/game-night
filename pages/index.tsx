1065113a84bbd5e2d385816d571c67c04fb3dad1
Antonio Lo Fiego <alofiego@hey.com>
2022-01-13 16:23:15 -0500

    Basic use of collection API route on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,44 +3,52 @@ import { supabase } from '@utils/supabase';
 import { Game } from '_types/Game';
 
 type PropType = {
-	collection: Game[];
+  collection: Game[];
 };
 
 const Home: React.FC<PropType> = ({ collection }) => {
-	console.log(collection);
-	return (
-		<>
-			<Head>
-				<title>GameNight</title>
-				<meta
-					name='description'
-					content='The easy way to organize your board game nights'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  // console.log(collection);
+  return (
+    <>
+      <Head>
+        <title>GameNight</title>
+        <meta
+          name='description'
+          content='The easy way to organize your board game nights'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-			<main>
-				<h1>GameNight</h1>
-			</main>
-		</>
-	);
+      <main>
+        <h1>GameNight</h1>
+        {collection.map((game, i) => {
+          return (
+            <div key={game.id}>
+              <p>{game.name}</p>
+              <p>{game.weight}</p>
+            </div>
+          );
+        })}
+      </main>
+    </>
+  );
 };
 
 export default Home;
 
 export const getServerSideProps = async () => {
-	const { data: userCollection, error } = await supabase
-		.from('userCollections')
-		.select(`boardgames(*)`)
-		.eq('user_id', 'c4814033-4979-4b10-9f0d-cf0b0c6cb4f5');
+  const { data: userCollection, error } = await supabase
+    .from('userCollections')
+    .select(`boardgames(*)`)
+    .eq('user_id', 'c4814033-4979-4b10-9f0d-cf0b0c6cb4f5');
 
-	console.log(userCollection?.length);
+  console.log(userCollection?.length);
 
-	const parsedCollection = userCollection?.map((game) => game.boardgames);
+  const parsedCollection = userCollection?.map((game) => game.boardgames);
 
-	return {
-		props: {
-			collection: parsedCollection,
-		},
-	};
+  return {
+    props: {
+      collection: parsedCollection,
+    },
+  };
 };

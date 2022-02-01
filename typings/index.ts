cd443b8620232017af6330b12f2821f6d99e6b44
Antonio Lo Fiego <alofiego@hey.com>
2022-02-01 15:09:30 -0500

    Export type SupabaseCollectionItem

--- typings/index.ts
+++ typings/index.ts
@@ -1,3 +1,4 @@
 export type { BGGBoardGame } from './BGGBoardGame';
 export type { Game } from './Game';
 export type { CollectionItem } from './CollectionItem';
+export type { SupabaseCollectionItem } from './SupabaseCollectionItem';

c0acaaf37e418fc1375bcdf30fe84defb7b1d4ac
Antonio Lo Fiego <alofiego@hey.com>
2022-02-01 23:30:05 -0500

    Temporary handling of auth on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useUser } from '@context/user';
 import Image from 'next/image';
+import Link from 'next/link';
 import axios from 'axios';
+import { Spinner } from '@components';
 
-import type { Game, SupabaseCollectionItem } from '@typings';
+import type { Game } from '@typings';
 
 const Home = () => {
   const { user, isLoading } = useUser();
@@ -17,7 +19,7 @@ const Home = () => {
       setCollection(() => data);
     };
 
-    if (!isLoading) {
+    if (!isLoading && user) {
       fetchData();
     }
   }, [user, isLoading]);
@@ -25,33 +27,56 @@ const Home = () => {
   return (
     <div>
       <h1>Home component</h1>
-      {isLoading ? (
-        <div>Loading...</div>
+      {isLoading && user ? (
+        <Spinner />
       ) : (
         <>
           <h1>{user?.bggUsername}</h1>
           <h2>{user?.email}</h2>
+          {user ? (
+            <Link href='/logout'>Logout</Link>
+          ) : (
+            <Link href='/authFlow'>Login</Link>
+          )}
+          {collection.length > 0 ? (
+            <div className='grid grid-cols-4'>
+              {collection.map((game) => {
+                return (
+                  <div
+                    key={game.bgg_id}
+                    className='grid items-center grid-cols-6'
+                  >
+                    <span className='relative w-12 h-12 col-span-1'>
+                      <Image
+                        src={game.thumbnail as string}
+                        alt={game.name}
+                        className='bg-white/15'
+                        objectFit='contain'
+                        layout='fill'
+                      />
+                    </span>
+                    <p className='col-span-4 truncate'>{game.name}</p>
+                    <p className='col-span-1 truncate'>{game.playCount}</p>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div>
+              No games to show. Wanna add some?{' '}
+              {user ? (
+                <p>Import your collection!</p>
+              ) : (
+                <Link href='/authFlow'>
+                  <a className='cursor-pointer hover:underline'>
+                    Login or create an account now
+                  </a>
+                </Link>
+              )}
+            </div>
+          )}
         </>
       )}
-      <div className='grid grid-cols-4'>
-        {collection.map((game) => {
-          return (
-            <div key={game.bgg_id} className='grid items-center grid-cols-6'>
-              <span className='relative w-12 h-12 col-span-1'>
-                <Image
-                  src={game.thumbnail as string}
-                  alt={game.name}
-                  className='bg-white/15'
-                  objectFit='contain'
-                  layout='fill'
-                />
-              </span>
-              <p className='col-span-4 truncate'>{game.name}</p>
-              <p className='col-span-1 truncate'>{game.playCount}</p>
-            </div>
-          );
-        })}
-      </div>
     </div>
   );
 };
